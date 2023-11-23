e2f928199c90c03015d3028bcd122592a969cdbb
haileyfritz <haileyfritz24@gmail.com>
2023-11-23 23:26:40 +0000

    new branch

--- App/models/student.py
+++ App/models/student.py
@@ -2,8 +2,12 @@ from App.models import User
 from App.database import db
 
 class Student(User):
-    id = db.Column(db.String(10), db.ForeignKey('user.id'), primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50))
+    
+    studentHistory = db.relationship('SemesterHistory', back_populates='student')
+    studentHistory = db.Column(db.Integer, db.ForeignKey('group.groupId'), nullable=False)
+
     program_id = db.Column(db.ForeignKey('program.id'))
     
     associated_program = db.relationship('Program', back_populates='students', overlaps="program")
