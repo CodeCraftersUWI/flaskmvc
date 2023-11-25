b7ab7130b68a259561a7f482f98ed7b473bbce96
Christin120 <98425721+Christin120@users.noreply.github.com>
2023-11-25 11:31:56 -0400

    Update course.py

--- App/models/course.py
+++ App/models/course.py
@@ -5,20 +5,29 @@ import json
 class Course(db.Model):
     
     courseCode = db.Column(db.String(8), primary_key=True)
-    prereqID = db.Column(db.Integer, db.ForeignKey('prerequisite.prereqID', nullable= False))
+    prereqID = db.Column(db.Integer, db.ForeignKey('prerequisite.prereqID'))
     courseName = db.Column(db.String(100), nullable=False)
     credits = db.Column(db.Integer, nullable=False, )
     difficulty = db.Column(db.Integer(20), nullable=False)
     
-    
-   
+    prerequisites = relationship('Course', backref=db.backref('prerequisite_for', remote_side=[courseCode]))
+
+   def add_prerequisite(self, prerequisite_code): 
+        prerequisite_course = Course.query.get(prerequisite_code)
+        if prerequisite_course:
+            self.prerequisites.append(prerequisite_course)
+            db.session.commit()
+
+    def edit_course(self, course_name, course_code, credits, difficulty):
+        self.courseName = course_name
+        self.courseCode = course_code
+        self.credits = credits
+        self.difficulty = difficulty
+        db.session.commit()
+
+    def __repr__(self):
+        return f"<Course {self.courseCode} - {self.courseName}>"
     
 
     
-    def get_json(self):
-        return{
-            'Course Code:': self.courseCode,
-            'Course Name: ': self.courseName,
-            'Course difficulty: ': self.difficulty,
-            'No. of Credits: ': self.credits,
-        }
+    
