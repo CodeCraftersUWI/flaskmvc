631e6595ebbb382009edfb9e7443eaec7544866c
Christin120 <98425721+Christin120@users.noreply.github.com>
2023-11-26 16:37:22 -0400

    Create semester.py

---
+++ App/models/semester.py
@@ -0,0 +1,22 @@
+from App.database import db
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.orm import relationship
+
+class Semester(db.Model):
+    semesterID= db.Column(db.Integer, primary_key = True) 
+    courses= db.relationship('SemesterCourse', backref=db.backref('course', lazy='joined'))
+    year= db.Column(db.Datetime, nullable = False)
+    semestertype= db.Column(db.Integer, nullable = False)
+
+    def __init__(self, year, semestertype):
+        self.year = year
+        self.semestertype = semestertype
+
+    def add_course(course):
+        if course not in self.courses:
+            self.sourses.append(course)
+            db.session.commit()
+
+    def remove_course(course):
+        self.courses.remove(course)
+        db.session.commit()
