49b0916d225af332c4707682d74968ad5860f3eb
Christin120 <98425721+Christin120@users.noreply.github.com>
2023-11-26 16:42:17 -0400

    Create semesterCourse.py

---
+++ App/models/semesterCourse.py
@@ -0,0 +1,20 @@
+from App.database import db
+from sqlalchemy import Column, Integer, ForeignKey
+from sqlalchemy.orm import relationship
+
+class SemesterCourse(db.Model):
+    __tablename__ = 'semester_courses'
+
+    semesterCourseID = Column(Integer, primary_key=True)
+    courseCode = Column(db.String(8), ForeignKey('courses.courseCode'), nullable=False)
+    semesterID = Column(Integer, ForeignKey('semesters.semesterID'), nullable=False)
+
+    semester = relationship('Semester', backref=db.backref('semester_courses'))
+    course = relationship('Course', backref=db.backref('semester_courses'))
+
+    def __init__(self, course_code, semester_id):
+        self.courseCode = course_code
+        self.semesterID = semester_id
+
+    def __repr__(self):
+        return f"<SemesterCourse {self.semesterCourseID}>"
