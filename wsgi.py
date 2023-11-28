96abba6143696d3c010cd1aebe7121d42f18eb8e
nicholasr74 <nicholasramroop74@gmail.com>
2023-11-28 04:31:56 +0000

    removed test comments in wsgi

--- wsgi.py
+++ wsgi.py
@@ -406,6 +406,3 @@ def allSemCourses():
 app.cli.add_command(course)
 
 
-
-#this is a test to play with merging from main to my own branch
-#lalalalalal another test comment to play with merging main to me pwon branch remember to delete lol
