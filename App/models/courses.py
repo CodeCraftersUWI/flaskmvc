52c836b3981e79114ac130428c7a375dce4034cc
Christin120 <98425721+Christin120@users.noreply.github.com>
2023-11-30 20:20:43 -0400

    Update courses.py

--- App/models/courses.py
+++ App/models/courses.py
@@ -3,29 +3,39 @@ from App.models import prerequisites
 import json
 
 class Course(db.Model):
+    
     courseCode = db.Column(db.String(8), primary_key=True)
-    courseName = db.Column(db.String(25))
-    credits = db.Column(db.Integer)
-    rating = db.Column(db.Integer)
-
-    offered = db.relationship('CoursesOfferedPerSem', backref ='courses', lazy=True)
-    students = db.relationship('StudentCourseHistory', backref='courses', lazy=True)
-    programs = db.relationship('ProgramCourses', backref='courses', lazy=True)
-    prerequisites = db.relationship('Prerequisites', backref='courses', lazy = True)
-
-    # planIds = db.relationship('CoursePlanCourses', backref='courses', lazy=True)
-   
+    prereqID = db.Column(db.Integer, db.ForeignKey('prerequisite.prereqID'))
+    courseName = db.Column(db.String(100), nullable=False)
+    credits = db.Column(db.Integer, nullable=False, )
+    difficulty = db.Column(db.Integer(20), nullable=False)
     
-    def __init__(self, code, name, rating, credits):
-        self.courseCode = code
-        self.courseName = name
-        self.rating = rating
+    prerequisites = relationship('Prerequisite', backref=db.backref('course',lazy='joined'))
+
+    def __init__(self, courseCode, prereqID, courseName, credits, difficulty):
+        self.courseCode = courseCode
+        self.prereqID = prereqID
+        self.courseName = courseName
         self.credits = credits
+        self.difficulty = difficulty
+
+
+   def add_prerequisite(self, prerequisite_code): 
+        prerequisite_course = Course.query.get(prerequisite_code)
+        if prerequisite_course:
+            self.prerequisites.append(prerequisite_course)
+            db.session.commit()
+
+    def edit_course(self, course_name, course_code, credits, difficulty):
+        self.courseName = course_name
+        self.courseCode = course_code
+        self.credits = credits
+        self.difficulty = difficulty
+        db.session.commit()
+
+    def __repr__(self):
+        return f"<Course {self.courseCode} - {self.courseName}>"
     
-    def get_json(self):
-        return{
-            'Course Code:': self.courseCode,
-            'Course Name: ': self.courseName,
-            'Course Rating: ': self.rating,
-            'No. of Credits: ': self.credits,
-        }
+
+    
+    
