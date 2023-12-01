7e3110a63c25a32ad1440060f1c3934fe25be52b
Christin120 <98425721+Christin120@users.noreply.github.com>
2023-12-01 19:57:11 -0400

    Update student.py

--- App/controllers/student.py
+++ App/controllers/student.py
@@ -2,55 +2,44 @@ from App.models import Student, CoursePlan, Program
 from App.controllers import (get_program_by_name)
 from App.database import db
 
-def create_student(student_id, password, name, programname):
-    program = get_program_by_name(programname)
-    if program:
-        new_student = Student(student_id, password, name, program.id)
-        db.session.add(new_student)
-        db.session.commit()
-        return new_student
-        print("Student successfully created")
-    else:
-        print("Program doesn't exist")
+# Controller to add a new student
+def add_student(studentID, first_name, last_name, email):
+    new_student = Student(, studentID=studentID, firstName=first_name, lastName=last_name, email=email)
+    db.session.add(new_student)
+    db.session.commit()
+    return new_student
 
-def get_student_by_id(ID):
-    return Student.query.filter_by(id=ID).first()
+# Controller to get a student by username (student_id)
+def get_student(studentID):
+    return Student.query.filter_by(studentID=student_studentID).first()
 
-def get_student(id):
-    return Student.query.get(id)
-
+# Controller to get a list of all students
 def get_all_students():
-    return Student.query.all()
-
-def get_all_students_json():
     students = Student.query.all()
-    if not students:
-        return []
-    students_json = [student.get_json() for student in students]
-    return students_json
+    student_data = [{'studentID': student.studentID, 'firstName': student.firstName, 'lastName': student.lastName, 'email': student.email} for student in students]
+    return student_data
 
-def update_student(id, username):
-    student = get_student_by_id(id)
+# Controller to get a list of students with a specific first name and last name
+def get_students_by_name(first_name, last_name):
+    students = Student.query.filter_by(firstName=first_name, lastName=last_name).all()
+    
+    # Create a list of dictionaries containing student data
+    student_search = [{'studentID': student.studentID, 'firstName': student.firstName, 'lastName': student.lastName, 'email':student.email} for student in students]
+    return student_search
+    
+# Controller to update a student
+def update_student(studentID, new_first_name, new_last_name, new_email):
+    student = Student.query.get(studentID)
     if student:
-        student.name = username
+        student.firstName = new_first_name
+        student.lastName = new_last_name
+        student.email = new_email
         db.session.add(student)
         db.session.commit()
         return student
+    return None
 
-def enroll_in_programme(student_id, programme_id):
-    student = get_student_by_id(student_id)
-    if student:
-        programme = Program.query.get(programme_id)
-        if programme:
-            student.program_id = programme_id
-            db.session.add(student)
-            db.session.commit()
-    return student.program_id
 
-def verify_student(username):
-    student=Student.query.filter_by(id=username).first()
-    if student:
-        return True
-    return False
+
 
 
