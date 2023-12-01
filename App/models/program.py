b15f97d24605bca8ad7073bf421b38a2ed4cdb4c
Christin120 <98425721+Christin120@users.noreply.github.com>
2023-11-30 20:18:21 -0400

    Update program.py

--- App/models/program.py
+++ App/models/program.py
@@ -1,27 +1,42 @@
 from App.database import db
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+
 class Program(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50))
-    core_credits = db.Column(db.Integer)
-    elective_credits = db.Column(db.Integer)
-    foun_credits = db.Column(db.Integer)
-    
-    students = db.relationship('Student', backref='program', lazy=True)
-    courses = db.relationship('ProgramCourses', backref='program', lazy=True)
+    __tablename__ = 'programs'
 
-    def __init__(self, name, core, elective, foun):
-       self.name = name
-       self.core_credits = core
-       self.elective_credits = elective
-       self.foun_credits = foun
+    programID = Column(Integer, primary_key=True)
+    departmentCode = Column(String(10), ForeignKey('departments.departmentCode'), nullable=False)
+    programName = Column(String, nullable=False)
+    coreCredits = Column(Integer, nullable=False)
+    electiveCredits = Column(Integer, nullable=False)
+    founCredits = Column(Integer, nullable=False)
 
+    coreCourses = db.relationship('ProgramCourse', sbackref=db.backref('course', lazy='joined'))
+    electiveCourses = db.relationship('ProgramCourse', backref=db.backref('course', lazy='joined'))
+    founCourses = db.relationship('ProgramCourse', backref=db.backref('course', lazy='joined'))
+    students = db.relationship('Student', backref=db.backref('student', lazy='joined'))
 
-    def get_json(self):
-        return{
-            'Program ID:': self.id,
-            'Program Name: ': self.name,
-            'Core Credits: ': self.core_credits,
-            'Elective Credits ': self.elective_credits,
-            'Foundation Credits: ': self.foun_credits,
-        }
-       
+    def __init__(self, department_code, program_name, core_credits, elective_credits, foun_credits):
+        self.departmentCode = department_code
+        self.programName = program_name
+        self.coreCredits = core_credits
+        self.electiveCredits = elective_credits
+        self.founCredits = foun_credits
+        
+    students = relationship('Student', backref='program')
+
+    def add_course(self, course_code, course_type):
+        
+        course = Course.query.get(course_code)
+        if course:
+            if course_type == 'core':
+                self.coreCourses.append(course)
+            elif course_type == 'elective':
+                self.electiveCourses.append(course)
+            elif course_type == 'foundation':
+                self.founCourses.append(course)
+            db.session.commit()
+
+    def __repr__(self):
+        return f"<Program {self.programID} - {self.programName}>"
