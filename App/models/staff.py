50de69d5f3e68e4e10ad7787b76eafe684e83e14
Christin120 <98425721+Christin120@users.noreply.github.com>
2023-11-30 20:16:35 -0400

    Update staff.py

--- App/models/staff.py
+++ App/models/staff.py
@@ -1,19 +1,43 @@
 from .user import User 
 from App.database import db
+from sqlalchemy import Column, Integer, Date, ForignKey
 
 class Staff(User):
-
     
-    id = db.Column(db.String(10), db.ForeignKey('user.id'), primary_key=True)
-    name = db.Column(db.String(50))
+    staffID = db.Column(db.Integer, primary_key=True)
+    departmentCode = db.Column(db.String(10), db.ForeignKey(department.departmentCode), nullable = False)
+    firstName = db.Column(db.String(50), nullable = False)
+    firstName = db.Column(db.String(50), nullable = False)
+    email = db.Column(db.String(254),nullable = False )
 
-    def __init__(self, password, staff_id, name):
-        super().__init__(staff_id, password)
-        self.id = staff_id
-        self.name = name
+    department = relationship('Department', backref='staff_members')
+    created_programs = relationship('Program', backref=db.backref('program', lazy='joined'))
+    created_courses = relationship('Course', backref=db.backref('course', lazy='joined'))
+    created_semesters = relationship('Semester', backref=db.backref('semester', lazy='joined'))
 
-    def get_json(self):
-        return{
-            'staff_id': self.id,
-            'name': self.name,
-        }
+    def create_program(self, departmentCode, programName, coreCredits, electiveCredits, foundCredits):
+        program = Program(self, departmentCode, programName, coreCredits, electiveCredits, foundCredits)
+        db.session.add(program)
+        db.session.commit()
+      
+    def create_course(self, courseCode, prereqID, courseName, credits, difficulty):
+        course = Course(self, courseCode, prereqID, courseName, credits, difficulty)
+        db.session.add(course)
+        db.session.commit()
+
+    def create_semester(self, year, semestertype):
+        semester = Semester(self, year, semestertype)
+        db.session.add(semester)
+        db.session.commit()
+
+    def remove_program(self, program_name):
+        program = Program.query.filter_by(programName=program_name, creator=self).first()
+        if program:
+            db.session.delete(program)
+            db.session.commit()
+            
+    def __repr__(self):
+        return f"<Staff {self.staffID} - {self.firstName} {self.lastName}>"
+       
+
+       
