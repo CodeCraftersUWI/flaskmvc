98cefa49732efca5704acfb9f478fd7896ab4a08
Christin120 <98425721+Christin120@users.noreply.github.com>
2023-12-02 02:39:03 -0400

    Update easiestCourses.py

--- App/controllers/easiestCourses.py
+++ App/controllers/easiestCourses.py
@@ -1,23 +1,20 @@
 from App.models import EasiestCourses, CoursePlan, Program, SemesterCourse, Student
 from App.database import db
 
-class EasiestCoursesController:
-    
-    def create_easiest_course_plan(student_id):
-        easiest_courses = EasiestCourses(student_id)
-        return easiest_courses
+def create_easiest_course_plan(student_id):
+  easiest_courses = EasiestCourses(student_id)
+  return easiest_courses
 
-    def update_easiest_course_plan(easiest_courses, semester_id, program_id, num_courses):
-        easiest_courses.reset(student_id)
-        easiest_courses.set_semester(semester_id)
-        easiest_courses.set_program(program_id)
-        easiest_courses.set_courses(num_courses)
+def update_easiest_course_plan(easiest_courses, semester_id, program_id, num_courses):
+    easiest_courses.reset(student_id)
+    easiest_courses.set_semester(semester_id)
+    easiest_courses.set_program(program_id)
+    easiest_courses.set_courses(num_courses)
 
-    
-    def get_easiest_course_plan(easiest_courses):
-        return easiest_courses.get_plan()
+def get_easiest_course_plan(easiest_courses):
+    return easiest_courses.get_plan()
 
-    def get_easiest_course_plan_json(easiest_courses):
-        return easiest_courses.get_json()
+def get_easiest_course_plan_json(easiest_courses):
+    return easiest_courses.get_json()
 
     
