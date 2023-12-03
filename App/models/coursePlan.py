30de8004d8864d7b194ab6c0897841b64b26eedd
Hex-006 <mjah0218@gmail.com>
2023-12-02 20:37:08 -0400

    models and necessar changes

--- App/models/coursePlan.py
+++ App/models/coursePlan.py
@@ -1,19 +1,27 @@
 from App.database import db
-
+from .plan_course_bridge import plan_course_bridge
 class CoursePlan(db.Model):
-    planId=db.Column(db.Integer, primary_key=True)
-    studentId=db.Column(db.Integer,  db.ForeignKey('student.id'), nullable=False)
-    
-    student = db.relationship('Student', backref=db.backref('course_plans', uselist=True))
+    __tablename__ ='CoursePlan'
+    id = db.Column(db.Integer, primary_key=True)
+    # studentId=db.Column(db.Integer,  db.ForeignKey('student.id'), nullable=False)
+    student = db.Column(db.Integer, db.ForeignKey("student.id"),nullable=False)
+    courses = db.relationship('course_plan_course',secondary=plan_course_bridge,backref='in_course_plans')
+    semester = db.Column(db.Integer, nullable=False)
+    year = db.Column(db.Integer, nullable=False)
 
-    # courses = db.relationship('CoursePlanCourses', backref = 'coursePlan', lazy=True)
-
-    def __init__(self, studentid, ):
-        self.studentId = studentid
+    def __init__(self, student, year,sem):
+        self.student = student
+        self.year = year
+        self.sem = sem
         
+    def get_plan_courses(self):
+        return [course.to_json() for course in self.courses]
 
-    def get_json(self):
+    def to_json(self):
         return{
-            'planId': self.planId,
-            'studentId': self.studentId,
+            'id': self.id,
+            'student_id': self.student,
+            'semester' : self.semester,
+            'year':self.year,
+            'plan_courses' : get_plan_courses()
         }
